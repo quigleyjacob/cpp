18a642b7a7209dc88ecdbf148d3af18d6ef27c0e
Jacob Quigley <quigleyj@wustl.edu>
2018-07-16 20:58:27 -0500

    Initial Commit

---
+++ cse332/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cse332
+//
+//  Created by Jacob Quigley on 7/16/18.
+//  Copyright © 2018 Jacob Quigley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
